3b0bfc2d9c898436a0d6f5dbe7cf22ac17d496b2
isabeldiana <isabel_diana@outlook.com>
2024-04-25 17:22:40 -0300

    create:  Criando a estrutura básica do Nest.js e criando o CRUD

---
+++ src/task/task.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { TaskDto } from './task.tdo';
+import { TaskService } from './task.service';
+
+@Controller('task')
+export class TaskController {
+  constructor(private readonly taskService: TaskService){}
+  @Post()
+  create(@Body() task: TaskDto){
+  this.taskService.create(task)
+  }
+  @Get('/:id')
+  findById(@Param('id') id:string): TaskDto{
+    return this.taskService.findById(id)
+  }
+@Put()
+update(@Body()  task: TaskDto){
+ this.taskService.update(task)
+}
+
+@Delete('/:id')
+remove(@Param('id') id: string){
+  return this.taskService.remove(id)
+}
+}
---
+++ src/task/task.service.ts
@@ -0,0 +1,39 @@
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { TaskDto } from './task.tdo';
+
+@Injectable()
+export class TaskService {
+  private tasks: TaskDto[] = [];
+  create(task: TaskDto){
+   this.tasks.push(task)
+   
+  }
+
+  findById(id: string): TaskDto{
+    const foundTask = this.tasks.filter(t => t.id === id);
+    if(foundTask.length){
+      return foundTask[0]
+    }
+    throw new NotFoundException(`Task with id ${id} not found`) 
+  }
+
+  update(task: TaskDto){
+   let taskIndex =  this.tasks.findIndex(t => t.id === task.id)
+
+   if(taskIndex >= 0){
+     this.tasks[taskIndex] = task;
+     return;
+   }
+   throw new HttpException(`Task with id ${task.id} not found`, HttpStatus.BAD_REQUEST) 
+  }
+
+  remove(id: string){
+    let taskIndex = this.tasks.findIndex(t => t.id === id);
+      if(taskIndex >= 0 ){
+        this.tasks.splice(taskIndex, 1);
+        return;
+      }
+
+      throw new HttpException(`Task with id ${id} not found`, HttpStatus.BAD_REQUEST) 
+  }
+}
