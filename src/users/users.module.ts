04ccc1f82da19299936c5d91a64c0cbfeee56b35
isabeldiana <isabel_diana@outlook.com>
2024-05-28 14:16:14 -0300

    refactoring: Refactoring users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/db/entities/users.entity';
 
 @Module({
   providers: [UsersService],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   exports: [UsersService],
   controllers: [UsersController]
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,17 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { UserDto } from './user.tdo';
 import { v4 as uuid } from 'uuid';
 import { hashSync as  bcryptHashSync } from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/db/entities/users.entity';
+import { Repository } from 'typeorm';
 @Injectable()
 export class UsersService {
   private readonly users: UserDto[] = []
 
-create(newUser: UserDto){
-  newUser.id = uuid();
-  newUser.password = bcryptHashSync(newUser.password,10);
-  this.users.push(newUser)
+   constructor(
+    @InjectRepository(UserEntity)
+    private readonly userReposiory: Repository<UserEntity>
+   ){ }
+async create(newUser: UserDto){
+ const userAlreadyRegistered = await this.findByUserName(newUser.username)
+ if(userAlreadyRegistered){
+  throw new ConflictException(`User '${newUser.username}' already registered`)
+ }
+
+ const dbUser = new UserEntity();
+ dbUser.username = newUser.username;
+ dbUser.password_hash = bcryptHashSync(newUser.password, 10);
+ const { id, username} = await this.userReposiory.save(dbUser)
+
+
+  return {id, username};
 }
-findByUserName(username: string): UserDto | null{
-  return this.users.find(user => user.username === username);
+async findByUserName(username: string): Promise<UserDto | null> {
+ const userfound = await this.userReposiory.findOne({
+  where: {username}
+ })
+
+ if(!userfound){
+ return  null;
+ }
+
+ return{
+  id: userfound.id,
+  username: userfound.username,
+  password: userfound.password_hash
+ }
 }
 }
